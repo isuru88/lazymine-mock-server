a4f9d2cd2a405188ce8edad2428023853f0f3a8b
zoneisuru <isuruj@zone24x7.com>
2015-06-24 19:52:59 +0530

    Added default route

--- server/controllers/app-controller.js
+++ server/controllers/app-controller.js
@@ -3,9 +3,27 @@ var logger = require('../logger.js'),
     request = require('request'),
 	current = require("../assets/current.json"),
 	time_entry_activities = require("../assets/time_entry_activities.json"),
-    issues = require("../assets/issues.json");    
+    issues = require("../assets/issues.json");  
+
+exports.default = function (req, res) {
+	var routes = req.app._router.stack,
+		response = '<h2>Available Routes</h2><ul>';
+	
+	for (var i = 0; i < routes.length; i++) {
+		if(routes[i].route) {
+			for (var j = 0; j < routes[i].route.stack.length; j++) {						
+				response += '<li>' + routes[i].route.stack[j].method.toUpperCase() + ' ' + routes[i].route.path + '</li>';			
+			}
+		}
+	}
+	
+	response += '</ul>';
+	
+	res.send(response);
+};
 	
 exports.getIssues = function (req, res) {
+	logger.log('Received request getIssues', req.query);
 	if(req.query.status_id === '1') {
 		res.send(issues);
 	}
@@ -15,13 +33,16 @@ exports.getIssues = function (req, res) {
 };
 
 exports.getCurrent = function (req, res) {
+	logger.log('Received request getCurrent', req.query);
 	res.send(current);
 };
 
 exports.getTimeEntryActivities = function (req, res) {
+	logger.log('Received request getTimeEntryActivities', req.query);
 	res.send(time_entry_activities);
 };
 
 exports.postTimeEntryActivities = function (req, res) {
-	res.status(201).send();
+	logger.log('Received request postTimeEntryActivities', req.query, req.body);
+	res.status(201).send({});
 };
--- server/routes/app-routes.js
+++ server/routes/app-routes.js
@@ -2,7 +2,8 @@
 
 var controller = require('../controllers/app-controller');
 
-exports.init = function(app){	
+exports.init = function(app){
+	app.route('/').get(controller.default);	
     app.route('/issues.json').get(controller.getIssues);
 	app.route('/users/current.json').get(controller.getCurrent);
 	app.route('/enumerations/time_entry_activities.json').get(controller.getTimeEntryActivities);
